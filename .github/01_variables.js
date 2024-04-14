bbe09ca4ee6ca6d56e67e8f929f1c6e874707261
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-04-14 18:18:40 +0000

    datatype

---
+++ .github/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 144553
+let accountemail = "anantsinggh@gmal.com"
+var accountPassword ="12345"
+accountcity = "jaipur"
+
+console.table([accountID, accountemail, accountPassword, accountcity])
+//accountID = 2 // not allowed
+
+accountemail = "aca.com"
+accountPassword = "13244124"
+accountcity = "goa"
+
+console.log(accountID);
+
+console.table([accountID, accountemail, accountPassword, accountcity])
