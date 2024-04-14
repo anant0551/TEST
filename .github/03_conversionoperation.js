b9c994ee2ea681c9ac53fcb7c3ea8f3312f16c30
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-04-14 18:51:11 +0000

    conversion of datatypes

---
+++ .github/03_conversionoperation.js
@@ -0,0 +1,29 @@
+//let score = "33"
+let score = "33abc"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true =>; false => 0
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsloggedIn);
+
+//1 => true ; 0=> false 
+// '' => false
+// "anant" => true
+
+
+let someNumber = 33
+let somestring = String(someNumber)
+console.log(somestring)
+console.log(typeof somestring)
