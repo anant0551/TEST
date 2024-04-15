862033005c7497fc32b316e50369f5cce9ca56ae
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-04-15 17:12:33 +0000

    main

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,68 @@
+//let score = "33"
+/*let score = "33abc"
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true =>; false => 0
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsloggedIn);
+
+//1 => true ; 0=> false 
+// '' => false
+// "anant" => true
+
+
+let someNumber = 33
+let somestring = String(someNumber)
+console.log(somestring)
+console.log(typeof somestring);
+*/
+//****************************operations*********************** *//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "anant"
+let str2 = "singh"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+ let gameCounter = 100
+ ++gameCounter; //perfix
+ gameCounter++;  //postfix
+ console.log(gameCounter);
+
