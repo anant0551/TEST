bce360a9ef2fd6c9526b75affc7c89436859edc3
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-04-15 17:36:26 +0000

    datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive (call by value)
+
+// 7 types  : String ,Number, Boolean, null, undefined, Symbol, BigInt(to store large number like scientific number)
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigumber = 12345678901234567893456n//BigInt syntax just put "n" in the last of the number
+
+
+
+// Reference (Non Premitive)
+
+// Array, objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+let myobj = {
+    name: "Anant",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+// console.log(typeof bigumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
+console.log(typeof heros);
