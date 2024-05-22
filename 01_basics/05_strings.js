4f2fc696c48a1d9220d7e507ad0e5e43f1919e2e
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-05-22 17:01:48 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "Anant"
+const repocount = 2
+
+//console.log(name + repocount + "value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String("Anant")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(2));
