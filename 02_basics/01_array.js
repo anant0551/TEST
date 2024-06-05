a82ac1bf6b2a438eae438b26a2f4bd254bb2beeb
anant0551 <120587367+anant0551@users.noreply.github.com>
2024-06-05 05:49:01 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,1 +1,36 @@
 //array
+const myarr = [0,1,2,3,4,5,6,7]
+const myHero = ["guddu badmaas", "bilota"]
+
+const myArr2 = new Array (1,2,3,4,5)
+// console.log(myarr[1]);
+
+
+// Array methods
+
+// myarr.push(9)
+// myarr.push(8)
+// myarr.pop()
+
+// myarr.unshift(10)
+// myarr.shift()
+// console.log(myarr.includes(10));
+// console.log(myarr.indexOf(3));
+
+// const newArr = myarr.join()
+
+// console.log(myarr);
+// console.log(newArr);
+
+// slice , splice
+
+console.log("A", myarr);
+
+const myn1 = myarr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myarr);
+
+const myn2 = myarr.splice(1,3)
+console.log("C",myarr);
+console.log(myn2);
